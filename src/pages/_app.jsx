f4f7e007e31a233d269536e8717a3e78ce224047
Rodrigo Martins de Oliveira <seuemaildopgithub@gmail.com>
2026-01-05 20:29:23 -0300

    Fix: ajustando caminho do CSS para pasta fora da src

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,4 @@
-import "../styles/globals.css"; // Verifique se o caminho do seu CSS está correto aqui
+import "../../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
