a31f4998dd2f63fd2d90db3d43f2abd63e3a7698
Rodrigo Martins de Oliveira <seuemaildopgithub@gmail.com>
2026-01-05 20:26:28 -0300

    Fix: configurando Tailwind para as pastas do Next.js

---
+++ src/pages/_app.jsx
@@ -0,0 +1,7 @@
+import "../styles/globals.css"; // Verifique se o caminho do seu CSS está correto aqui
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
